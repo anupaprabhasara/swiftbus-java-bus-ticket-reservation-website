889c305913dd78300cad5d9525f3fd30f61fd9ee
Anupa Prabhasara <info@anupa.lk>
2025-04-26 15:13:39 +0530

    first commit

---
+++ src/main/java/com/swiftbus/util/ConnectionData.java
@@ -0,0 +1,8 @@
+package com.swiftbus.util;
+
+public class ConnectionData {
+
+    public static final String DATABASE = "swiftbus_db";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "123456";
+}
